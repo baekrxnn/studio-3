6378aa05666c80850116ff90c04369d7ed620c5a
Glitch (react-native-web-playground) <none>
2017-12-28 13:11:42 +0000

    🌎🎊 Checkpoint

--- src/App/__tests__/index.test.js
+++ src/App/__tests__/index.test.js
@@ -1,8 +1,10 @@
+/* eslint-env jasmine, jest */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '..';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
