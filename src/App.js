f056e4adaeeb17293c9fd7d96d00ab389df831e6
Glitch (react-native-web-playground) <none>
2020-04-23 00:15:22 +0000

    🐧💄 Checkpoint ./package.json:789040/3968 ./src/App.js:789040/1022

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+// see https://github.com/necolas/react-native-web
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi!</h1>
+      </div>
+    );
+  }
+}
+
+
+export default App;
