098951a7f431857d4cb8ea4468d51bad492516c1
Glitch (react-native-web-playground) <none>
2020-04-23 00:39:22 +0000

    👗🏤 Checkpoint ./src/App.js:789040/100 ./index.test.js:789040/22

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import InputBox from './InputBox.js';
 
-
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>
@@ -12,5 +12,3 @@ class App extends Component {
   }
 }
 
-
-export default App;
