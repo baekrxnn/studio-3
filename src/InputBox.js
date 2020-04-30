77bde1667fc2a6b90be5d3273d3e1ccdbd8288b6
Glitch (react-native-web-playground) <none>
2020-04-30 01:16:20 +0000

    🍈🐕 Checkpoint ./src/InputBox.js:789040/691 ./src/Word.js:789040/33

--- src/InputBox.js
+++ src/InputBox.js
@@ -5,10 +5,34 @@ export default class ImportBox extends Component{
       super();
     }
     
+  
+    question = () => {
+        let colors = [
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "purple",
+            "black"
+        ];
+
+        let promptColor = Math.floor(Math.random()*colors.length);
+        let promptWord = Math.floor(Math.random()*colors.length);
+        this.setState ({
+            color: colors[promptColor],
+            wording: colors[promptColor],
+        })
+        
+    }
+    
     answerCheck = (value) => {
+        let prompt = this.props.prompt;
         let color = this.props.color;
         let word = this.props.word;
-        
+        if (prompt === "color" ) {
+            console.log(color);
+        }
     }
     
     render() {
@@ -16,7 +40,7 @@ export default class ImportBox extends Component{
           <div className="box">
               <h1> Type the word/color </h1>
               <input placeholder="your answer here"></input>
-              <button> submit </button>
+              <button onClick={this.answerCheck()}> submit </button>
           </div>
         )
     }
--- src/Word.js
+++ src/Word.js
@@ -38,7 +38,7 @@ export default class Word extends Component {
         return (
             <div>
                 <h1 style={{color:this.state.color}}>{this.state.wording}</h1>
-                <InputBox color={this.state.color} word={this.state.word}/>
+                <InputBox prompt={this.state.prompt} color={this.state.color} word={this.state.word}/>
             </div>
             
         );
